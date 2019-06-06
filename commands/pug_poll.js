1a133a9884e536f8faaef5004d8029b68795ddab
aalberg <aralberg@gmail.com>
2019-06-05 22:38:25 -0700

    More pug_poll cleanup

--- commands/pug_poll.js
+++ commands/pug_poll.js
@@ -445,7 +445,7 @@ module.exports = {
     } else if (args[0] === 'post') {
       postPoll(new Date());
     } else if (args[0] === 'delete') {
-      deletePoll();
+      message.channel.send('Not yet implemented');
     } else if (args[0] === 'announce') {
       announcePug(new Date().getDay());
     }
